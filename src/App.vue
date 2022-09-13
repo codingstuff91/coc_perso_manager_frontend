cd3e3354dd27dc807f454e6f80569f001da01f6d
Matthieu MARTIN <codingstuff91@gmail.com>
2022-09-13 19:34:49 +0200

    Import Navbar and custom css styles

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Navbar/>
+  <div class="container">
+    <router-view/>
+  </div>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  components: {
+    Navbar,
+  },
+}
+</script>
+
 <style>
+*{
+  padding: 0;
+  margin: 0;
+}
+
 #app {
+  width: 100%;
+  margin-top: 100px;
+  /* border: 2px solid red; */
+  margin: 0 auto;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  background-color: #e9ecf1;
+  /* color: #2c3e50; */
 }
 
 nav {
@@ -26,5 +45,13 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.container{
+  padding-top: 70px;
+  max-width: 80%;
+  height: 100vh;
+  margin: 0 auto;
+  /* border: 2px solid blue; */
 }
 </style>
