22d9cef156385214b5cb69e85125cd8980d03570
Matthieu MARTIN <codingstuff91@gmail.com>
2022-09-13 19:33:59 +0200

    Create base input and button components

---
+++ src/components/forms/BaseButton.vue
@@ -0,0 +1,40 @@
+<template>
+    <button>
+        {{ text }}
+    </button>
+</template>
+
+<script>
+    export default {
+        props: {
+            text: {
+                type: String,
+                default: ''
+            },
+        },
+    }
+</script>
+
+<style lang="css" scoped>
+button{
+    border-radius: 10px;
+    height: 2em;
+    font-size: 1.2em;
+    width: 30%;
+    background-image: linear-gradient(90deg, #3454FB 0%, #7636FF 100%);
+    color: #ffff;
+    cursor: pointer;
+}
+
+@media screen and (max-width: 400px) {
+    button{
+        border-radius: 10px;
+        height: 2.5em;
+        font-size: 1.2em;
+        width: 80%;
+        background-image: linear-gradient(90deg, #3454FB 0%, #7636FF 100%);
+        color: #ffff;
+        cursor: pointer;
+    }
+}
+</style>
---
+++ src/components/forms/BaseInput.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="form_input">
+        <label v-if="label">{{ label }}</label>
+        <input :type="$attrs" v-model="modelValue" @input="$emit('update:modelValue', $event.target.value)">
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            label: {
+                type: String,
+                default: ''
+            },
+            modelValue : {
+                type : [ String, Number ],
+                default : ''
+            }
+        },
+    }
+</script>
+
+<style lang="css" scoped>
+    .form_input{
+        margin: 1.2rem 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    .form_input label{
+        font-size: 1.5em;
+    }
+    .form_input input{
+        margin : 1em 0;
+        border : none;
+        padding : 0 1em;
+        font-size: 1em;
+        height: 2em;
+        width: 25%;
+        border-radius: 5px;
+    }
+
+    .form_input input:focus{
+        border: 2px solid red;
+    }
+
+@media screen and (max-width: 400px) {
+    .form_input input{
+        margin : 0.7em 0;
+        border : none;
+        padding : 0 1em;
+        font-size: 1em;
+        height: 2.5em;
+        width: 80%;
+        border-radius: 5px;
+    }
+}
+</style>
