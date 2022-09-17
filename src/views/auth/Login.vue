58ecd43ffa9c378dc9a3c9c8389916689ca3d186
Matt MARTIN <codingstuff91@gmail.com>
2022-09-17 21:41:33 +0200

    User login

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,15 +1,61 @@
 <template>
-    <div>
-        <h1>Hello from login</h1>
-    </div>
+    <h1>Connexion</h1>
+
+    <BaseInput 
+        type="text" 
+        label="Adresse email" 
+        v-model="email"
+    />
+
+    <BaseInput 
+        type="password" 
+        label="Mot de passe" 
+        v-model="password"
+    />
+
+    <BaseButton 
+            text="Connexion" 
+            @click="submitLogin"
+        />
+    
+    <small class="subtitle">Pas encore de compte ? Inscrivez vous ici : <button><a href="/register">Inscription</a></button></small>
 </template>
 
 <script>
+import AuthService from '@/services/AuthService';
+import BaseInput from '@/components/forms/BaseInput.vue';
+import BaseButton from '@/components/forms/BaseButton.vue';
+
     export default {
-        
+        data() {
+            return {
+                email: '',
+                password : ''
+            }
+        },
+        components: {
+            BaseInput,
+            BaseButton
+        },
+        methods: {
+            submitLogin() {
+                AuthService.login({
+                    email : this.email, 
+                    password : this.password})
+                .then(({data}) => {
+                    localStorage.setItem('token', data);
+                })
+            }
+        },
     }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="css" scoped>
+h1{
+    margin:30px auto;
+}
+.subtitle{
+    display: block;
+    margin: 10px auto;
+}
 </style>
