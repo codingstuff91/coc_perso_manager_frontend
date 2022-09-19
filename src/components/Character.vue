e5b57092df10574745521ed9a0e4531acdbc383c
Matt MARTIN <codingstuff91@gmail.com>
2022-09-19 19:31:37 +0200

    First design of characterIndex view page

---
+++ src/components/Character.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="character">
+        <div class="character-header">
+            <img src="../assets/man.png" width="50">
+            <h1>James BOND</h1>
+        </div>
+        <div class="character-footer">
+            <button>Consulter</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="css" scoped>
+.character{
+    background-color: #4c4bc4;
+    padding : 10px;
+    border-radius: 20px;
+    box-shadow: 5px 5px 15px 5px #ABABAB;
+
+}
+.character-header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width : 65%;
+    margin: 0 auto;
+}
+
+.character img{
+    border: 2px solid #e9ecf1;
+    border-radius: 50px;
+    padding: 10px;
+}
+
+.character-header h1{
+    font-size: 20px;
+    font-weight: bold;
+    color: #ffff;
+}
+.character-footer button{
+    cursor: pointer;
+    padding: 10px;
+    border-radius: 10px;
+    margin: 20px 0;
+    background-color: #ffff;
+    width : 80%;
+    font-size: 16px;
+    font-weight: bold;
+}
+</style>
--- src/views/CharacterIndex.vue
+++ src/views/CharacterIndex.vue
@@ -1,27 +1,45 @@
 <template>
-    <div>
-        <h1>Personnages disponibles</h1>
+    <div class="title">
+        <img src="../assets/character.png" width="50">
+        <h1>Personnages jouables</h1>
     </div>
+    <Character />
 </template>
 
 <script>
 import CharacterService from '@/services/CharacterService';
+import Character from '@/components/Character.vue';
 
     export default {
-        methods: {
-            getCharacters() {
-                CharacterService.getCharacters().then(response => console.log(response.data))
-            }
-        },
-        mounted () {
-            this.getCharacters();
-        },
+    methods: {
+        getCharacters() {
+            CharacterService.getCharacters().then(response => console.log(response.data));
+        }
+    },
+    mounted() {
+        this.getCharacters();
+    },
+    components: { 
+        Character, 
     }
+}
 </script>
 
 <style lang="css" scoped>
-h1{
+.title{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: #4c4bc4;
+    padding : 10px;
+    border-radius: 10px;
+    box-shadow: 5px 5px 15px 5px #ABABAB;
     margin: 20px 0;
+}
+
+h1{
     font-size: 18px;
+    color: #ffff;
+    margin-top: 10px;
 }
 </style>
