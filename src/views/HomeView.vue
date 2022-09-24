d61e9b154ebabf8d9079e2f96c4ed4659d745dff
Matt MARTIN <codingstuff91@gmail.com>
2022-09-24 15:25:19 +0200

    Edit Homeview component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,32 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/coc_logo.jpg">
-    <h1>Gérez simplement vos personnages de jeu de roles</h1>
-    <h2>Pour le jeu de roles "Chroniques oubliées Contemporain"</h2>
+  <div>
+    <h1 class="pt-10 text-xl font-bold text-center">
+      Gérez simplement vos personnages pour le jeu de roles "Chroniques oubliées Contemporain"
+    </h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
 }
 </script>
-
-<style lang="css" scoped>
-img{
-  width: 300px;
-  height: auto;
-  margin: 0 auto;
-}
-
-h1{
-  margin: 20px 0;
-}
-
-</style>
