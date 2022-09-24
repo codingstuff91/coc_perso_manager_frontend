2f509729bd63d578625739075ce47612202321bb
Matt MARTIN <codingstuff91@gmail.com>
2022-09-24 16:06:35 +0200

    BaseButton replace styles by tailwindcss

--- src/components/forms/BaseButton.vue
+++ src/components/forms/BaseButton.vue
@@ -1,5 +1,5 @@
 <template>
-    <button>
+    <button class="p-4 bg-slate-600 text-white rounded-lg w-[100%] text-xl mt-4">
         {{ text }}
     </button>
 </template>
@@ -14,27 +14,3 @@
         },
     }
 </script>
-
-<style lang="css" scoped>
-button{
-    border-radius: 10px;
-    height: 2em;
-    font-size: 1.2em;
-    width: 30%;
-    background-image: linear-gradient(90deg, #3454FB 0%, #7636FF 100%);
-    color: #ffff;
-    cursor: pointer;
-}
-
-@media screen and (max-width: 400px) {
-    button{
-        border-radius: 10px;
-        height: 2.5em;
-        font-size: 1.2em;
-        width: 80%;
-        background-image: linear-gradient(90deg, #3454FB 0%, #7636FF 100%);
-        color: #ffff;
-        cursor: pointer;
-    }
-}
-</style>
