22014d6b339164df84f22e386b49f7e17e545130
Matthieu MARTIN <codingstuff91@gmail.com>
2022-09-25 18:17:30 +0200

    Get and display all characters and edit Character component

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,12 +1,13 @@
 <template>
     <div class="rounded-lg p-4 bg-slate-400 w-[90%] mx-auto drop-shadow-2xl">
-        <div class="flex justify-between items-center w-[140px] mx-auto">
-            <img src="../assets/man.png"  class="mr-4" width="50">
-            <h1 class="text-white font-bold">James BOND</h1>
+        <div class="flex justify-center items-center w-[80%] mx-auto">
+            <img v-if="characterInfos.gender == 'M'" src="../assets/man.png"  class="mr-4" width="55">
+            <img v-else src="../assets/girl.png" class="mr-4" width="55">
+            <h1 class="text-white text-xl font-bold">{{ characterInfos.name }}</h1>
         </div>
         <div class="flex justify-center">
             <button class="mt-4 p-2 rounded-lg w-[70%] bg-white font-bold text-lg drop-shadow-lg">
-                Consulter
+                <router-link class="nav-link" :to="`/character/${characterInfos.id}`">Consulter</router-link>
             </button>
         </div>
     </div>
@@ -14,6 +15,10 @@
 
 <script>
     export default {
-        
+        props: {
+            characterInfos: {
+                type: Object
+            },
+        },
     }
 </script>
--- src/views/CharacterIndex.vue
+++ src/views/CharacterIndex.vue
@@ -1,6 +1,10 @@
 <template>
-    <SubNavbar title="Personnage"/>
-    <Character />
+    <SubNavbar title="Personnages"/>
+    <Character 
+        class="my-2" 
+        v-for="character in characters" :key="character.id"
+        :characterInfos="character"
+    />
 </template>
 
 <script>
@@ -9,36 +13,24 @@ import Character from '@/components/Character.vue';
 import SubNavbar from '@/components/SubNavbar.vue';
 
     export default {
-    methods: {
-        getCharacters() {
-            CharacterService.getCharacters().then(response => console.log(response.data));
+        data() {
+            return {
+                characters : []
+            }
+        },
+        methods: {
+            getCharacters() {
+                CharacterService.getCharacters().then(response => { 
+                    this.characters = response.data;
+                });
+            }
+        },
+        mounted() {
+            this.getCharacters();
+        },
+        components: {
+            Character,
+            SubNavbar
         }
-    },
-    mounted() {
-        this.getCharacters();
-    },
-    components: {
-    Character,
-    SubNavbar
-}
 }
 </script>
-
-<style lang="css" scoped>
-/* .title{
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: #4c4bc4;
-    padding : 10px;
-    border-radius: 10px;
-    box-shadow: 5px 5px 15px 5px #ABABAB;
-    margin: 20px 0;
-}
-
-h1{
-    font-size: 18px;
-    color: #ffff;
-    margin-top: 10px;
-} */
-</style>
