b646755ce86795048eab006c05b0cf6503b99ba7
Matt MARTIN <codingstuff91@gmail.com>
2022-09-25 13:21:27 +0200

    Isolate character vitality component

---
+++ src/components/CharacterVitality.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="w-[95%] mx-auto mt-2">
+        <div class="card">
+            <h1 class="card-header">Vitalité</h1>
+            <div class="columns-2 gap-2 mt-2">
+                <div class="text-center text-sm text-white bg-black p-2 rounded-md w-full flex flex-col justify-between">
+                    <p>Points de vie (PV)</p>
+                    <p>10</p>
+                </div>
+                <div class="text-center text-sm text-white bg-black p-2 rounded-md w-full flex flex-col justify-between">
+                    <p>Dé de vie</p>
+                    <p>10</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/CharacterShow.vue
+++ src/views/CharacterShow.vue
@@ -1,56 +1,25 @@
 <template>
     <div>
         <SubNavbar title="James BOND"/>
-        <div class="w-[95%] mx-auto">
-            <div class="card">
-                <h1 class="card-header">Caractéristiques</h1>
-                <div class="columns-2 gap-4 flex justify-between">
-                    <div class="flex justify-between mt-2 p-2 rounded-md text-sm text-white bg-black w-full">
-                        <p>Force</p>
-                        <p>18 / +2</p>
-                    </div>
-                    <div class="flex justify-between mt-2 p-2 rounded-md text-white bg-black w-full">
-                        <p>Dextérité</p>
-                        <p>10 / 0</p>
-                    </div>
-                </div>
-                <div class="columns-2 gap-2 flex justify-between">
-                    <div class="flex justify-between mt-2 p-2 rounded-md text-sm text-white bg-black w-full">
-                        <p>Constitution</p>
-                        <p>18 / +2</p>
-                    </div>
-                    <div class="flex justify-between mt-2 p-2 rounded-md text-sm text-white bg-black w-full">
-                        <p>Intelligence</p>
-                        <p>10 / 0</p>
-                    </div>
-                </div>
-                <div class="columns-2 gap-2 flex justify-between">
-                    <div class="flex justify-between mt-2 p-2 rounded-md text-sm text-white bg-black w-full">
-                        <p>Perception</p>
-                        <p>18 / +2</p>
-                    </div>
-                    <div class="flex justify-between mt-2 p-2 rounded-md text-sm text-white bg-black w-full">
-                        <p>Charisme</p>
-                        <p>10 / 0</p>
-                    </div>
-                </div>
-            </div>
-            <div class="card mt-2">
-                <h1 class="card-header">Vitalité / Chance</h1>
-                <div class="columns-3">
-                    <p class="text-center text-md text-white bg-black p-2">PV : 10</p>
-                    <p class="text-center text-md text-white bg-black p-2">DV : 1D10</p>
-                    <p class="text-center text-md text-white bg-black p-2">PC : 4</p>
-                </div>
-            </div>
-        </div>
+
+        <CharacterSkills/>
+
+        <CharacterVitality/>
+
     </div>
 </template>
 
 <script>
 import SubNavbar from '@/components/SubNavbar.vue';
+import CharacterSkills from '../components/CharacterSkills.vue';
+import CharacterVitality from '@/components/CharacterVitality.vue';
+
     export default {
-    components: { SubNavbar }
+    components: {
+        SubNavbar,
+        CharacterSkills,
+        CharacterVitality
+    }
 }
 </script>
 
