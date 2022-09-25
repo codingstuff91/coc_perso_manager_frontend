80ee381409323a2bc16522f95041864bf6ddfefc
Matthieu MARTIN <codingstuff91@gmail.com>
2022-09-25 17:46:30 +0200

    Create character capacities component

---
+++ src/components/CharacterCapacities.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="w-[95%] mx-auto mt-2">
+        <div class="card">
+            <h1 class="card-header">Capacités / Voies</h1>
+            <div class="mt-2 bg-black p-2 rounded-lg">
+                <h2 class="text-white font-bold text-center mb-2">Voie des corporations</h2>
+                <div class="flex text-white columns-2 my-2">
+                    <p class="w-1/3">Niveau 1 :</p>
+                    <p class="w-2/3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore delectus a quas porro, eligendi incidunt ipsum ab, modi nam alias magnam soluta atque reprehenderit dicta nobis nulla culpa quae. Nam!</p>
+                </div>
+            </div>
+            <div class="mt-2 bg-black p-2 rounded-lg">
+                <h2 class="text-white font-bold text-center mb-2">Voie du danger</h2>
+                <div class="flex text-white columns-2">
+                    <p class="w-1/3">Niveau 1 :</p>
+                    <p class="w-2/3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore delectus a quas porro, eligendi incidunt ipsum ab, modi nam alias magnam soluta atque reprehenderit dicta nobis nulla culpa quae. Nam!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/CharacterShow.vue
+++ src/views/CharacterShow.vue
@@ -9,6 +9,8 @@
         <CharacterCombatSkills/>
 
         <CharacterWeapons/>
+
+        <CharacterCapacities/>
     </div>
 </template>
 
@@ -18,15 +20,17 @@ import CharacterSkills from '../components/CharacterSkills.vue';
 import CharacterVitality from '@/components/CharacterVitality.vue';
 import CharacterCombatSkills from '@/components/CharacterCombatSkills.vue';
 import CharacterWeapons from '@/components/CharacterWeapons.vue';
+import CharacterCapacities from '@/components/CharacterCapacities.vue';
 
     export default {
-    components: {
-    SubNavbar,
-    CharacterSkills,
-    CharacterVitality,
-    CharacterCombatSkills,
-    CharacterWeapons,
-}
+        components: {
+            SubNavbar,
+            CharacterSkills,
+            CharacterVitality,
+            CharacterCombatSkills,
+            CharacterWeapons,
+            CharacterCapacities,
+        }
 }
 </script>
 
