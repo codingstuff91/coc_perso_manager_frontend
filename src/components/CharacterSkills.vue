639a65c0a3f93ed275ca299848cadb67a9304b8d
Matt MARTIN <codingstuff91@gmail.com>
2022-09-25 13:01:02 +0200

    Isolate Character skills in separate component

---
+++ src/components/CharacterSkills.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="w-[95%] mx-auto">
+        <div class="card">
+            <h1 class="card-header">Caractéristiques</h1>
+            <h2 class="text-center text-md text-white">(Valeur / Modificateur)</h2>
+            <div class="columns-2 gap-4 flex justify-between">
+                <div class="flex justify-between mt-2 p-2 rounded-md text-sm text-white bg-black w-full">
+                    <p>Force</p>
+                    <p>18 / +2</p>
+                </div>
+                <div class="flex justify-between mt-2 p-2 rounded-md text-white bg-black w-full">
+                    <p>Dextérité</p>
+                    <p>10 / 0</p>
+                </div>
+            </div>
+            <div class="columns-2 gap-2 flex justify-between">
+                <div class="flex justify-between mt-2 p-2 rounded-md text-sm text-white bg-black w-full">
+                    <p>Constitution</p>
+                    <p>18 / +2</p>
+                </div>
+                <div class="flex justify-between mt-2 p-2 rounded-md text-sm text-white bg-black w-full">
+                    <p>Intelligence</p>
+                    <p>10 / 0</p>
+                </div>
+            </div>
+            <div class="columns-2 gap-2 flex justify-between">
+                <div class="flex justify-between mt-2 p-2 rounded-md text-sm text-white bg-black w-full">
+                    <p>Perception</p>
+                    <p>18 / +2</p>
+                </div>
+                <div class="flex justify-between mt-2 p-2 rounded-md text-sm text-white bg-black w-full">
+                    <p>Charisme</p>
+                    <p>10 / 0</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
