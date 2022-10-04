b36b3b241c6d973a3edba99c659807d10126db8f
Matthieu MARTIN <codingstuff91@gmail.com>
2022-10-04 22:02:43 +0200

    Display combat character properties

--- src/components/CharacterCombatSkills.vue
+++ src/components/CharacterCombatSkills.vue
@@ -5,17 +5,17 @@
             <div class="columns-2 gap-2 mt-2">
                 <div class="text-center text-sm text-white bg-black p-2 rounded-md w-full flex justify-between">
                     <p>Initiative</p>
-                    <p>10</p>
+                    <p>{{ characterInfos.attributes[1].pivot.value }}</p>
                 </div>
                 <div class="text-center text-sm text-white bg-black p-2 rounded-md w-full flex justify-between">
                     <p>Att au contact</p>
-                    <p>10</p>
+                    <p>{{ formatModificator(characterInfos.attributes[0].pivot.modificator) }}</p>
                 </div>
             </div>
             <div class="columns-2 gap-2 mt-2">
                 <div class="text-center text-sm text-white bg-black p-2 rounded-md w-full flex justify-between">
                     <p>Att à dist</p>
-                    <p>10</p>
+                    <p>{{ formatModificator(characterInfos.attributes[1].pivot.modificator) }}</p>
                 </div>
                 <div class="text-center text-sm text-white bg-black p-2 rounded-md w-full flex justify-between">
                     <p>Att mentale</p>
@@ -24,7 +24,7 @@
             </div>
             <h2 class="card-header mt-2">Défense</h2>
             <div class="mt-2 text-center text-sm text-white bg-black p-2 rounded-md w-full flex justify-between">
-                <p>Base : 10</p>
+                <p>Base : {{ parseInt(10) + parseInt(characterInfos.attributes[1].pivot.modificator) }}</p>
                 <p>Bonus : +2</p>
                 <p>Total : 12</p>
             </div>
@@ -33,8 +33,17 @@
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
     export default {
-        
+        computed: mapState({
+            characterInfos : state => state.character
+        }),  
+        methods: {
+            formatModificator(value) {
+                return value > 0 ? `+${value}` : value;
+            }
+        },
     }
 </script>
 
