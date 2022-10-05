0b75e122d54f63a3721589fb30885c8ecf714ab2
Matthieu MARTIN <codingstuff91@gmail.com>
2022-10-05 22:32:05 +0200

    Edit character capacities

--- src/components/CharacterCapacities.vue
+++ src/components/CharacterCapacities.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="w-[95%] mx-auto mt-2">
         <div class="card">
-            <h1 class="card-header">Capacités / Voies</h1>
+            <h1 class="card-header">Capacités</h1>
             <div class="mt-2 bg-black p-2 rounded-lg" v-for="capacity in capacities" :key="capacity.id">
-                <h2 class="text-white font-bold text-center mb-2" >{{ capacity[0].name }}</h2>
+                <h2 class="text-white font-bold text-center mb-2 text-lg" >{{ capacity[0].name }}</h2>
                 <div class="flex text-white columns-2 my-2" v-for="item in capacity" :key="item">
-                    <p class="w-1/3">Niv {{ item.level }}:</p>
-                    <p class="w-2/3">{{ item.description }}</p>
+                    <p class="w-1/3 mt-2">Niv {{ item.level }}</p>
+                    <p class="w-2/3 mt-2">{{ item.description }}</p>
                 </div>
             </div>
         </div>
