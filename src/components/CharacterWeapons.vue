73d3609a06a2c05a9d085b4c9c228795339f2c74
Matthieu MARTIN <codingstuff91@gmail.com>
2022-10-05 21:46:07 +0200

    Character weapon list

--- src/components/CharacterWeapons.vue
+++ src/components/CharacterWeapons.vue
@@ -2,17 +2,22 @@
     <div class="w-[95%] mx-auto mt-2">
         <div class="card">
             <h1 class="card-header">Armes</h1>
-            <div class="mt-2 text-center text-sm text-white bg-black p-2 rounded-md w-full flex justify-between">
-                <p>Arme : 10</p>
-                <p>Attaque : 1D20 + </p>
-                <p>DM : 1D3</p>
+            <h2 class="text-center text-white text-sm">Nom / Jet attaque / Jet de dégats</h2>
+            <div class="mt-2 text-center text-sm text-white bg-black p-2 rounded-md w-full flex justify-between" v-for="weapon in weapons" :key="weapon.id">
+                <p>{{ weapon.name }}</p>
+                <p>{{ weapon.attack_score }}</p>
+                <p>{{ weapon.dm_score }}</p>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
     export default {
-        
+        computed: mapState({
+            weapons : state => state.character.weapons
+        }),    
     }
 </script>
