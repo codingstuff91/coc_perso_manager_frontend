fa803d762537b8a17c5be53042b5758c6819731f
Matthieu MARTIN <codingstuff91@gmail.com>
2023-01-07 18:52:32 +0100

    Add createCapacity child component

--- src/views/admin/CapacitiesIndex.vue
+++ src/views/admin/CapacitiesIndex.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="mx-auto w-[70%]">
         <h1 class="pt-5 text-center text-3xl text-bold">Gestion des capacités</h1>
+        <CapacityCreate v-if="addCapacity" />
+        <div class="flex justify-center">
+            <button 
+                class="btn"
+                @click="addCapacity = !addCapacity"
+            >
+                Ajouter une capacité
+            </button>
+        </div>
         <p 
             class="clickable-card mt-4 text-center text-xl p-4 bg-white rounded-lg" 
             v-for="capacity in capacities" 
@@ -13,8 +22,18 @@
 
 <script>
 import { mapState } from 'vuex'
+import CapacityCreate from '@/views/admin/CapacityCreate.vue';
+import BaseButton from '@/components/forms/BaseButton.vue';
 
     export default {
+        data() {
+            return {
+                addCapacity: false
+            }
+        },
+        components: {
+            CapacityCreate,
+        },
         computed: mapState({
             capacities : state => state.capacities,
         }),
